7591f286cfb5563faeb8e2cab61144c4c5d83c24
R. Tyler Julian <trjulian@gmail.com>
2016-01-31 12:29:47 -0800

    feat: add String.to_base64() by casting the input string
    We cast the immutable Lua string type to a `char *` in order to
    use it in `ngx_encode_base64`. Care should be taken to prevent the
    casted reference from leaking, which could cause undefined behavior.

--- lib/resty/string.lua
+++ lib/resty/string.lua
@@ -16,12 +16,21 @@ local _M = { _VERSION = '0.09' }
 ffi.cdef[[
 typedef unsigned char u_char;
 
+typedef struct {
+    size_t len;
+    u_char *data;
+} ngx_str_t;
+
 u_char * ngx_hex_dump(u_char *dst, const u_char *src, size_t len);
 
 intptr_t ngx_atoi(const unsigned char *line, size_t n);
+
+void ngx_encode_base64(ngx_str_t *dst, ngx_str_t *src);
 ]]
 
 local str_type = ffi.typeof("uint8_t[?]")
+local str_ptr = ffi.typeof("char *")
+local ngx_str_type = ffi.typeof("ngx_str_t")
 
 
 function _M.to_hex(s)
@@ -29,6 +38,15 @@ function _M.to_hex(s)
     local buf = ffi_new(str_type, len)
     C.ngx_hex_dump(buf, s, #s)
     return ffi_str(buf, len)
+end
+
+
+function _M.to_base64(s)
+    local len = math.floor(#s * 4 / 3 + 0.5)  -- convert byte length to base-64
+    local src = ffi_new(ngx_str_type, #s, ffi.cast(str_ptr, s))
+    local dst = ffi_new(ngx_str_type, len, ffi_new(str_type, len))
+    C.ngx_encode_base64(dst, src)
+    return ffi_str(dst.data, dst.len)
 end
 
 
